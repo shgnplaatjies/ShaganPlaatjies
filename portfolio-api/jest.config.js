0c811784ca74871372dfd5044d819b17b0517353
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-26 16:46:07 +0200

    Clears project, restarting in Next.js

--- portfolio-api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.spec.ts"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  setupFiles: ["<rootDir>/src/setupTests.ts"],
-};
