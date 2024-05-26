d72de4a021f2d952c7133233685d390f158b6d8e
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-26 22:00:20 +0200

    Project initialization. - Installs and initializes:   - Next.js + deps   - Tailwind + deps   - Stitches/react   - RadixUI

---
+++ shaganplaatjies-site/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Shagan Plaatjies",
+  description:
+    "My slice of the silicone sea, etching my multi-threaded journey in code.  Hello, world! L'appel du vide, will you answer? ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
