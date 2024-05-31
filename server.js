9cc7d8baae390ee345808642763546a99a2f8b7f
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-31 21:34:05 +0200

    Adds back server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const nextjs = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = nextjs({ dev });
+const nextHandler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("*", (req, res) => {
+    return nextHandler(req, res);
+  });
+
+  server.listen(4444, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:4444");
+  });
+});
