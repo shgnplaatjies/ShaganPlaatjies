3f9be67afa9701432554c69e3d666cb316b77922
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-31 16:46:21 +0200

    Removes custom environments

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 import cnad from "@bitc/cnad";
-import { config } from "dotenv";
-
-config({
-  path: () => {
-    switch (process.env.NODE_ENV) {
-      case "production":
-        return ".env.production";
-      case "staging":
-        return ".env.staging";
-      case "local":
-        return ".env.local";
-      default:
-        throw new Error(
-          "NODE_ENV must be set to 'staging', 'production', or 'local'"
-        );
-    }
-  },
-});
 
 const nextConfig = {};
 
