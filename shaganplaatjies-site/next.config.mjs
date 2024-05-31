3e58f39900a9a8f28e7a2de7408452ca13a1ad29
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-31 13:59:40 +0200

    Adds automated deployment with CNAD library by BITC

--- shaganplaatjies-site/next.config.mjs
+++ shaganplaatjies-site/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+import cnad from "@bitc/cnad";
+import { config } from "dotenv";
+
+config({
+  path: () => {
+    switch (process.env.NODE_ENV) {
+      case "prod":
+        return ".env.prod";
+      case "stg":
+        return ".env.stg";
+      case "local":
+        return ".env.local";
+      default:
+        throw new Error("NODE_ENV must be set to 'dev', 'prod', or 'local'");
+    }
+  },
+});
+
 const nextConfig = {};
+
+cnad.config(process.env.APP_ROOT_DIR);
+cnad.watch([process.env.RESTART_FILE_PATH]);
+cnad.start();
 
 export default nextConfig;
