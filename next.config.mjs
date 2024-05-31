015fdf552a5e541335dc8a3f78e7072b4783be48
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-05-31 16:40:52 +0200

    CHanges use of environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -6,21 +6,23 @@ import { config } from "dotenv";
 config({
   path: () => {
     switch (process.env.NODE_ENV) {
-      case "prod":
-        return ".env.prod";
-      case "stg":
-        return ".env.stg";
+      case "production":
+        return ".env.production";
+      case "staging":
+        return ".env.staging";
       case "local":
         return ".env.local";
       default:
-        throw new Error("NODE_ENV must be set to 'dev', 'prod', or 'local'");
+        throw new Error(
+          "NODE_ENV must be set to 'staging', 'production', or 'local'"
+        );
     }
   },
 });
 
 const nextConfig = {};
 
-cnad.config(process.env.APP_ROOT_DIR);
+cnad.config(process.env.NODE_DIR);
 cnad.watch([process.env.RESTART_FILE_PATH]);
 cnad.start();
 
