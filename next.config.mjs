264375d19aa5f572c3e13e10432487a918a86978
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-02 00:22:24 +0200

    Moves cnad from Next.js config to Express config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import cnad from "@bitc/cnad";
-
 const nextConfig = {};
-
-cnad.config(process.env.NODE_DIR);
-cnad.watch([process.env.RESTART_FILE_PATH]);
-cnad.start();
 
 export default nextConfig;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const nextjs = require("next");
 
+cnad.config(process.env.NODE_DIR);
+cnad.watch([process.env.RESTART_FILE_PATH]);
+cnad.start();
+
 const dev = process.env.NODE_ENV !== "production";
 const app = nextjs({ dev });
 const nextHandler = app.getRequestHandler();
