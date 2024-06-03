85d5e68f8f419edb9edc74250bc1d190e62ac8fe
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-03 13:11:22 +0200

    Changes font Inter>Poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shagan Plaatjies",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Theme>{children}</Theme>
       </body>
     </html>
