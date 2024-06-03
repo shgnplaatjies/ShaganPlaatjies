9d2e35bec65377169af95a5afc0ef42778946956
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-03 11:33:35 +0200

    Adds missing cnad import

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nextjs = require("next");
+const cnad = require("cnad");
 
 cnad.config(process.env.NODE_DIR);
 cnad.watch([process.env.RESTART_FILE_PATH]);
