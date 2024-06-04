e788f1861caa574506433c0b212fe34ce91df432
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-04 15:56:42 +0200

    Removes cors middleware (test)

--- middleware.ts
+++ middleware.ts
@@ -1,60 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/profile", "/settings"]);
-
-const corsOptions: {
-  allowedMethods: string[];
-  allowedOrigins: string[];
-  allowedHeaders: string[];
-  exposedHeaders: string[];
-  maxAge?: number;
-  credentials?: boolean;
-} = {
-  allowedMethods: (process.env.ALLOWED_METHODS || "").split(","),
-  allowedOrigins: (process.env.ALLOWED_ORIGINS || "").split(","),
-  allowedHeaders: (process.env.ALLOWED_HEADERS || "").split(","),
-  exposedHeaders: (process.env.EXPOSED_HEADERS || "").split(","),
-  maxAge: (process.env?.MAX_AGE && parseInt(process.env.MAX_AGE)) || undefined,
-  credentials: process.env?.CREDENTIALS == "true",
-};
-
-export function middleware(req: NextRequest) {
-  const response = NextResponse.next();
-
-  const origin = req.headers.get("origin") ?? "";
-
-  if (
-    corsOptions.allowedOrigins.includes("*") ||
-    corsOptions.allowedOrigins.includes(origin)
-  )
-    response.headers.set("Access-Control-Allow-Origin", origin);
-
-  if (corsOptions.credentials)
-    response.headers.set(
-      "Access-Control-Allow-Credentials",
-      corsOptions.credentials.toString()
-    );
-
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    corsOptions.allowedMethods.join(",")
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    corsOptions.allowedHeaders.join(",")
-  );
-  response.headers.set(
-    "Access-Control-Expose-Headers",
-    corsOptions.exposedHeaders.join(",")
-  );
-  response.headers.set(
-    "Access-Control-Max-Age",
-    corsOptions.maxAge?.toString() ?? ""
-  );
-
-  return response;
-}
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
