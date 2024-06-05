0ebf349e328ed60ab8d109da1a22235b16fac223
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-05 16:38:46 +0200

    Changes implementation: middleware.ts>next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,32 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  headers: async () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Origin",
+          value: process.env.ALLOWED_ORIGIN,
+        },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: process.env.ALLOWED_METHODS,
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: process.env.ALLOWED_HEADERS,
+        },
+        {
+          key: "Access-Control-Allow-Exposed-Headers",
+          value: process.env.EXPOSED_HEADERS,
+        },
+        { key: "Access-Control-Allow-Max-Age>", value: process.env.MAX_AGE },
+        {
+          key: "Access-Control-Allow-Credentials",
+          value: process.env.CREDENTIALS === "true" ? "true" : "false",
+        },
+      ],
+    },
+  ],
+};
