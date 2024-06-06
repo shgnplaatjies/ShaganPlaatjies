671ad6f279591924023b2a0e59a1af1c3134c219
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-06 04:40:05 +0200

    Removing cors - Might re-implement custom cors headers when needed, I'd rather skip adding 6 new environment variables to the github deployment workflow for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,3 @@
 /** @type {import('next').NextConfig} */
 
-export default {
-  headers: async () => [
-    {
-      source: "/api/:path*",
-      headers: [
-        {
-          key: "Access-Control-Allow-Origin",
-          value: process.env.ALLOWED_ORIGIN,
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: process.env.ALLOWED_METHODS,
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value: process.env.ALLOWED_HEADERS,
-        },
-        {
-          key: "Access-Control-Allow-Exposed-Headers",
-          value: process.env.EXPOSED_HEADERS,
-        },
-        { key: "Access-Control-Allow-Max-Age>", value: process.env.MAX_AGE },
-        {
-          key: "Access-Control-Allow-Credentials",
-          value: process.env.CREDENTIALS === "true" ? "true" : "false",
-        },
-      ],
-    },
-  ],
-};
+export default {};
