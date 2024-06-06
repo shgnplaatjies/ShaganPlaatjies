231b191a802dd388743090162835c86bc34d36ce
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-06 04:28:34 +0200

    Updates server.js to use HTTPS for Express server locally - Adds dynamic dotenv (necessary due to some local issues) - Adds error handling to server.js entry Adds

--- server.js
+++ server.js
@@ -1,30 +1,55 @@
 const express = require("express");
 const nextjs = require("next");
 const cnad = require("@bitc/cnad");
+const https = require("https");
+const dotenv = require("dotenv");
 
-const env = env;
-if (env === "staging" || env === "development" || env === "production") {
-  cnad.config(process.env.NODE_DIR);
-  cnad.watch([process.env.RESTART_FILE_PATH]);
-  cnad.start();
-}
+dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const env = process.env.NODE_ENV;
+
+cnad.config(process.env.NODE_DIR);
+cnad.watch([process.env.RESTART_FILE_PATH]);
+cnad.start();
+
+const httpsOptions = {
+  key: process.env.SSL_CERT_KEY,
+  cert: process.env.SSL_CERT_CRT,
+};
 
 const dev = env !== "production";
 const app = nextjs({ dev });
 
 const nextHandler = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.get("*", (req, res) => {
-    return nextHandler(req, res);
+    server.all("*", (req, res) => {
+      return nextHandler(req, res);
+    });
+
+    const port = process.env.APP_PORT;
+
+    if (env === "production")
+      server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(
+          `> Ready with Express on http://localhost:${process.env.APP_PORT}`
+        );
+      });
+    else
+      https.createServer(httpsOptions, server).listen(port, (err) => {
+        if (err) throw err;
+        console.log(
+          `> Ready with Express + Local SSL on https://localhost:${process.env.APP_PORT}`
+        );
+      });
+  })
+  .catch((error) => {
+    console.error("Error initializing application:", error);
+    process.exit(1);
   });
-
-  const port = process.env.APP_PORT;
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${process.env.APP_PORT}`);
-  });
-});
