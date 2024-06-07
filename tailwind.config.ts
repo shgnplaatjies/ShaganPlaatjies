b038caa46a4cdfcf47c74d06e9eea94dcaca6ca7
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-07 21:59:41 +0200

    Removes CSS and tailwind bloat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,7 @@ const config: Config = {
     "./node_modules/@radix-ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
