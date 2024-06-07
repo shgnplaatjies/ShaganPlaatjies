85cc6c01071ed71eff010934e62127581cdcb4eb
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-07 22:00:35 +0200

    Adds dark mode to theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Flex, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Poppins } from "next/font/google";
 import MainLayout from "./components/layout/Main/layout";
 import "./styles/globals.css";
@@ -22,15 +23,18 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={poppins.className}>
-          <Theme
-            accentColor="green"
-            radius="small"
-            panelBackground="translucent"
-          >
-            <Flex direction="column" height="100vh" width="100vw">
-              <MainLayout>{children}</MainLayout>
-            </Flex>
-          </Theme>
+          <ThemeProvider attribute="media">
+            <Theme
+              appearance="dark"
+              accentColor="green"
+              radius="small"
+              panelBackground="translucent"
+            >
+              <Flex direction="column" height="100vh" width="100vw">
+                <MainLayout>{children}</MainLayout>
+              </Flex>
+            </Theme>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
