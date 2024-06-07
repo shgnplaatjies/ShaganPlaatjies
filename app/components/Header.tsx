81454fada080455ba89257b0e6202a0f8c68b074
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-07 22:01:24 +0200

    Updates header to use Navigation hamburger dropdown

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,15 @@
+import { CodeIcon } from "@radix-ui/react-icons";
+import { Flex, Text } from "@radix-ui/themes";
 import React from "react";
-import Navbar from "./Navbar";
+import NavButton from "./NavButton";
 
 const Header: React.FC = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <Flex direction="row" height="2rem" overflow={"visible"} justify="between">
+      <CodeIcon />
+      <Text as="p">Shagan Plaatjies</Text>
+      <NavButton />
+    </Flex>
   );
 };
 
