194512848db8b75e55fb53fc2830c626108cc2c5
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-07 03:11:47 +0200

    Adds social links as constants

---
+++ app/lib/constants.ts
@@ -0,0 +1,11 @@
+export const CONTACT_INFO = {
+  email: "hello@shaganplaatjies.co.za",
+  phone: "+27765527856",
+};
+
+export const SOCIAL_LINKS = {
+  github: "https://github.com/shgnplaatjies",
+  linkedin: "https://linkedin.com/in/shaganplaatjies",
+  email: `mailto:${CONTACT_INFO.email}`,
+  phone: `tel:${CONTACT_INFO.phone}`,
+};
