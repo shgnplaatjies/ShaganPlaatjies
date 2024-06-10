03489a19d15369690e596f2a1f27f528b9fd94ff
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-10 21:21:21 +0200

    Adds radix-ui colors to tailwind config and package.json

---
+++ tailwind.theme.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const generateScale = (name: string) => {
+  let scale = Array.from({ length: 12 }, (_, i) => {
+    let id = i + 1;
+    return [
+      [id, `var(--${name}${id})`],
+      [`a${id}`, `var(--${name}A${id})`],
+    ];
+  }).flat();
+
+  return Object.fromEntries(scale);
+};
+
+const themeConfig: Config = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        gray: generateScale("slate"),
+        accent: generateScale("green"),
+      },
+    },
+  },
+};
+
+export default themeConfig;
