1a03da958023d4e04dcc5a419cd7a9aba561c611
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-20 17:03:24 +0200

    Adds custom fonts to theme and optimized font.ts service.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import "../public/styles/globals.css";
 import MainLayout from "./components/layout/Main/layout";
-import { UbuntuMono } from "./lib/fonts";
+import { SpaceMono } from "./lib/fonts";
 
 export const metadata: Metadata = {
   title: "Shagan Plaatjies",
@@ -19,13 +19,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={UbuntuMono.className}>
+        <body>
           <Theme
             appearance="dark"
             accentColor="green"
             radius="small"
+            className={`flex flex-col h-screen w-screen ${SpaceMono.className}`}
             panelBackground="translucent"
-            className="flex flex-col h-screen w-screen"
           >
             <Box className=" flex flex-col h-full w-full backdrop-blur-3xl bg-gradient-to-br">
               <Box className="flex w-full h-full p-3">
--- app/lib/fonts.ts
+++ app/lib/fonts.ts
@@ -1,16 +1,19 @@
-import { Fira_Code, Ubuntu_Mono } from "next/font/google";
+import { Chakra_Petch, Space_Mono } from "next/font/google";
 import localFont from "next/font/local";
-
-export const UbuntuMono = Ubuntu_Mono({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-ubuntu-mono",
-});
-
-export const FiraCode = Fira_Code({ subsets: ["latin"], display: "swap" });
 
 export const FixedSys = localFont({
   src: "./fonts/FixedSys.ttf",
+  display: "swap",
+});
+
+export const SpaceMono = Space_Mono({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const ChakraPetch = Chakra_Petch({
+  weight: "700",
+  subsets: ["latin"],
   display: "swap",
 });
