e6797f5747f8353cd16021ba847c26b5f1161ec4
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-20 16:14:30 +0200

    Adds custom local heading fonts. Optimized to load only where used.

--- app/lib/fonts.ts
+++ app/lib/fonts.ts
@@ -1,4 +1,5 @@
 import { Fira_Code, Ubuntu_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
 export const UbuntuMono = Ubuntu_Mono({
   weight: "400",
@@ -7,4 +8,9 @@ export const UbuntuMono = Ubuntu_Mono({
   variable: "--font-ubuntu-mono",
 });
 
-export const FiraCode = Fira_Code({ subsets: ["latin"] });
+export const FiraCode = Fira_Code({ subsets: ["latin"], display: "swap" });
+
+export const FixedSys = localFont({
+  src: "./fonts/FixedSys.ttf",
+  display: "swap",
+});
