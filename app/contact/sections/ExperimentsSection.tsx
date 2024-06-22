950194ddf33c28c0fd9fd43d38ae5c2db306535f
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-22 20:36:42 +0200

    Adds placeholder content for all pages sections

---
+++ app/contact/sections/ExperimentsSection.tsx
@@ -0,0 +1,32 @@
+import { CodeIcon } from "@radix-ui/react-icons";
+import { Section, Text } from "@radix-ui/themes";
+import AccentedHeading from "../../components/AccentedHeading";
+import WindowLayout from "../../components/Window";
+
+const AboutSection: React.FC = () => {
+  return (
+    <Section>
+      <AccentedHeading
+        textAs="h2"
+        size="8"
+        preText="My "
+        accentedText="Story."
+      />
+      <Text as="p">
+        Allow me to introduce myself, my work and my journey in software.
+      </Text>
+      <WindowLayout title="about me" padded LogoIcon={<CodeIcon />}>
+        <Text as="p">Technologies</Text>
+        <ul>
+          <li>React</li>
+          <li>TailwindCSS</li>
+          <li>ExpressJs</li>
+          <li>NextJs</li>
+        </ul>
+      </WindowLayout>
+      <CTAButton href="/about" text="About Me" />
+    </Section>
+  );
+};
+
+export default AboutSection;
