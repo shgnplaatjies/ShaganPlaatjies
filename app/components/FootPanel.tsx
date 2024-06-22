cd85e8c7e66fb5ef3beea31d6a1c37c60984ebe4
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-22 12:34:36 +0200

    Replaces footpanel's nav icon list with current location and time.

--- app/components/FootPanel.tsx
+++ app/components/FootPanel.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { DotFilledIcon } from "@radix-ui/react-icons";
-import { Box, Flex, Grid, Link } from "@radix-ui/themes";
-import React from "react";
+import { Box, Grid, Link } from "@radix-ui/themes";
+import React, { useEffect, useState } from "react";
+import { POLLING_INTERVAL } from "../lib/constants";
 import IconList from "./IconsList";
-import { getNavIcons } from "./icons/NavIcons";
 import { getSocialIcons } from "./icons/SocialIcons";
 
 type FootPanelProps = {
@@ -14,23 +14,33 @@ const FootPanel: React.FC<FootPanelProps> = ({
   className = "",
 }: FootPanelProps) => {
   const socialIcons = getSocialIcons();
-  const navIcons = getNavIcons();
+
+  const getTime = () =>
+    new Date().toLocaleTimeString("en-ZA", {
+      hour12: true,
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+  const [currentTime, setCurrentTime] = useState<string>(getTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(getTime());
+    }, POLLING_INTERVAL);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Box className={className}>
       <Grid width="100%" columns="3">
-        <Flex justify="start">
-          <Link href="/contact" className="flex justify-start" asChild>
-            <DotFilledIcon color="green" width="1.25rem" height="1.25rem" />
-          </Link>
-        </Flex>
-
-        <Flex justify="center">
-          <IconList icons={navIcons} />
-        </Flex>
-
-        <Flex justify="end">
-          <IconList icons={socialIcons} />
-        </Flex>
+        <Link href="/contact" asChild>
+          <DotFilledIcon color="green" width="1.25rem" height="1.25rem" />
+        </Link>
+        <p className="flex text-nowrap justify-center opacity-60 text-sm">
+          Johannesburg {currentTime}
+        </p>
+        <IconList className="flex justify-end" icons={socialIcons} />
       </Grid>
     </Box>
   );
