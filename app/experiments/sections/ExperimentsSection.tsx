dcf26f6dde9282d46047c227e8ab7412d4818c04
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-22 21:19:36 +0200

    Adds experiments page's placeholder sections

--- app/experiments/sections/ExperimentsSection.tsx
+++ app/experiments/sections/ExperimentsSection.tsx
@@ -1,33 +1,11 @@
-import { CodeIcon } from "@radix-ui/react-icons";
-import { Section, Text } from "@radix-ui/themes";
-import AccentedHeading from "../../components/AccentedHeading";
-import CTAButton from "../../components/CTAButton";
-import WindowLayout from "../../components/Window";
+import { Heading, Section } from "@radix-ui/themes";
 
-const AboutSection: React.FC = () => {
+const ExperimentsSection: React.FC = () => {
   return (
     <Section>
-      <AccentedHeading
-        textAs="h2"
-        size="8"
-        preText="My "
-        accentedText="Story."
-      />
-      <Text as="p">
-        Allow me to introduce myself, my work and my journey in software.
-      </Text>
-      <WindowLayout title="about me" padded LogoIcon={<CodeIcon />}>
-        <Text as="p">Technologies</Text>
-        <ul>
-          <li>React</li>
-          <li>TailwindCSS</li>
-          <li>ExpressJs</li>
-          <li>NextJs</li>
-        </ul>
-      </WindowLayout>
-      <CTAButton href="/about" text="About Me" />
+      <Heading>Experiments Section</Heading>
     </Section>
   );
 };
 
-export default AboutSection;
+export default ExperimentsSection;
