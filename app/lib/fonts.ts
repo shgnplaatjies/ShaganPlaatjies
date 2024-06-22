5c04babbe29de2db2ea741085ffc216c12203e4d
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-22 17:19:49 +0200

    Optimizes fonts by adding fallbacks.

--- app/lib/fonts.ts
+++ app/lib/fonts.ts
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 export const FixedSys = localFont({
   src: "./fonts/FixedSys.ttf",
   display: "swap",
+  fallback: ["monospace", "sans-serif"],
   variable: "--font-fixed-sys",
 });
 
@@ -11,6 +12,7 @@ export const SpaceMono = Space_Mono({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
+  fallback: ["monospace", "sans-serif"],
   variable: "--font-space-mono",
 });
 
@@ -18,5 +20,6 @@ export const ChakraPetch = Chakra_Petch({
   weight: "700",
   subsets: ["latin"],
   display: "swap",
+  fallback: ["Inter", "sans-serif"],
   variable: "--font-chakra-petch",
 });
