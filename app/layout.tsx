5d5faf41005b46b7ea5ac2e03110e0939d1104c5
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-23 12:27:01 +0200

    Adds gray as default color.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,7 @@ export default function RootLayout({
         className={`${SpaceMono.variable} ${FixedSys.variable} ${ChakraPetch.variable}`}
       >
         <body className="flex flex-col h-dvh w-dvh overflow-hidden">
-          <Theme
-            appearance="dark"
-            accentColor="grass"
-            radius="small"
-            panelBackground="translucent"
-          >
+          <Theme appearance="dark" radius="small" panelBackground="translucent">
             <Box className=" flex flex-col h-full p-3 w-full backdrop-blur-3xl bg-gradient-to-br">
               <MainLayout>{children}</MainLayout>
             </Box>
