4b23db5b8ad1bd2a48f65eaa93a77c9e0b81a556
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-24 19:40:39 +0200

    Adds detect OS middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/profile", "/settings"]);
 
@@ -8,4 +9,14 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/api/:path*"],
+};
+
+export const detectOS = (request: NextRequest) => {
+  const { os } = userAgent(request);
+
+  const response = NextResponse.next();
+
+  response.headers.set("x-user-os", os.name || "unknown");
+
+  return response;
 };
