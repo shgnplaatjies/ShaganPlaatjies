5d4a7630545d7ab9198932fcec67fc1b304ea0a5
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-06-26 22:00:15 +0200

    Adds Media fetching and displays taxonomy lists using component.
    Adds taxonomy list component to display taxonomies.
    
    Cuts out year from the date.

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,5 +1,8 @@
-import { Flex } from "@radix-ui/themes";
+import { Box, Flex, Heading, Text } from "@radix-ui/themes";
+import Image from "next/image";
 import React from "react";
+import { DefaultFeaturedImage } from "../lib/constants";
+import TaxonomyList from "./TaxonomyList";
 
 export type BlogPostExcerpt = {
   id: number;
@@ -7,7 +10,7 @@ export type BlogPostExcerpt = {
   modifiedGmt: string;
   slug: string;
   status: string;
-  link: string;
+  link: string; // url
   titleRendered: string;
   featuredMedia?: string; // url
   categories?: string[];
@@ -44,11 +47,35 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   },
 }) => {
   return (
-    <Flex>
-      <p>{id}</p>
-      <h1>{titleRendered}</h1>
-      <p>{excerptRendered}</p>
-    </Flex>
+    <Box mt={"8"}>
+      <Flex
+        direction="column"
+        px="3"
+        className="rounded border border-gray-border-1"
+        pb="5"
+      >
+        <Flex justify="center" mt={"-8"} mb="4" height="10rem">
+          <Image
+            className="rounded border border-gray-border-1"
+            src={featuredMedia}
+            width={400}
+            height={200}
+            alt={"Some Image and stuff"}
+          />
+        </Flex>
+        <Flex direction="column" gap="3" mx="3">
+          <Flex justify="between" className="opacity-60">
+            <Text size="2">{id < 10 ? `0${id}` : id}</Text>
+            <Text size="2">{dateGmt.slice(0, 4)}</Text>
+          </Flex>
+          <Heading wrap="wrap" as="h2">
+            {titleRendered}
+          </Heading>
+          {categories && <TaxonomyList taxonomies={categories} />}
+          {tags && <TaxonomyList taxonomies={tags} />}
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
 
