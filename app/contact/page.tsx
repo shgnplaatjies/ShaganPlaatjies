5cf9972d29fb97e6ab590990dc0583818d8e7f52
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2025-02-18 22:32:03 +0200

    Bugfiex and backtesting

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ import { Section } from "@radix-ui/themes";
 import { ContactForm } from "../components/ContactForm";
 import { Footer } from "../components/Footer";
 import LandingSection from "./sections/LandingSection";
+
 const ContactPage: React.FC = () => {
   return (
     <>
