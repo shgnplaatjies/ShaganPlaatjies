3f067bc9e787d560362671f5fc61ed34e554a4f1
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2025-11-20 19:25:39 +0200

    Updates colors

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -40,7 +40,7 @@ export type OrbColorOnPagesConfigType = {
 
 export const OrbColorOnPagesConfig: OrbColorOnPagesConfigType = {
   default: { path: "", color: "fill-radix-base-cyan", radixColor: "cyan" },
-  home: { path: "/", color: "fill-radix-base-grass", radixColor: "grass" },
+  home: { path: "/", color: "fill-radix-base-cyan", radixColor: "cyan" },
   about: {
     path: "/about",
     color: "fill-radix-base-tomato",
@@ -68,8 +68,8 @@ export const OrbColorOnPagesConfig: OrbColorOnPagesConfigType = {
   },
   contact: {
     path: "/contact",
-    color: "fill-radix-base-sky",
-    radixColor: "sky",
+    color: "fill-radix-base-cyan",
+    radixColor: "cyan",
   },
 };
 
