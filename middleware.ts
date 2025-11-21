3fbe24f3c56f45f661c0eb6a074f8633278c3740
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2025-11-21 16:05:29 +0200

    Removes unneeded and broken clerk auth implementation

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse, userAgent } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/profile", "/settings"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/api/:path*"],
-};
-
-export const detectOS = (request: NextRequest) => {
-  const { os } = userAgent(request);
-
-  const response = NextResponse.next();
-
-  response.headers.set("x-user-os", os.name || "unknown");
-
-  return response;
 };
