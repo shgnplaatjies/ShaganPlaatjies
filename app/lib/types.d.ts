588ee71f09f15c1c10bd104601e9ed2bdd0c7c95
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2025-11-22 21:31:29 +0200

    Updates footer

--- app/lib/types.d.ts
+++ app/lib/types.d.ts
@@ -1,9 +1,3 @@
-// declare module "@strudel.cycles/core";
-// declare module "@strudel.cycles/mini";
-// declare module "@strudel.cycles/tonal";
-// declare module "@strudel.cycles/webaudio";
-// declare module "@strudel.cycles/react";
-
 declare module "@strudel/core" {
   export function repl(config: any): any;
   export function note(...args: any[]): any;
